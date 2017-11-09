46ab27daf1d28fcc7e2e2af522572a8b7beb32d5
Chieh C "Jack" Chen <spdrcd@gmail.com>
2017-11-09 15:42:55 -0600

    added dataset calculation score

--- Question 11/bagging.py
+++ Question 11/bagging.py
@@ -1,3 +1,19 @@
+
+#some resources used
+
+#scikit documentation for bagging
+#http://scikit-learn.org/stable/modules/ensemble.html#bagging
+
+#youtube tutorial for scikit learn ensemble methods
+#https://www.youtube.com/watch?v=NqdyfMbVo1Q&t=5s
+
+#Github tutorial for ensemble methods
+#https://github.com/knathanieltucker/bit-of-data-science-and-scikit-learn/blob/master/notebooks/EnsembleMethods.ipynb
+
+#Team Members
+#Chieh Chen ppsid 0837931
+#Sidharth Sadani
+
 from sklearn.ensemble import BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -6,7 +22,7 @@ import numpy as np
 #using this for the other data set needed -- scikit leanr already included dataset
 import sklearn.datasets as data_sets
 #using the iris dataset from scikit learn
-irisX, irisY = data_sets.load_iris(return_X_y=True)
+Iris_X, Iris_y = data_sets.load_iris(return_X_y=True)
 
 
 contents = []
@@ -17,7 +33,7 @@ with open("iosphere_data.txt") as f:
         contents.append([s for s in line.strip().split(',')])
 
 X = [[float(p) for p in ex[:-1]] for ex in contents]
-Y = [1.0 if sublist[-1] == 'g' else 0.0 for sublist in contents]
+Y = [True if sublist[-1] == 'g' else False for sublist in contents]
 print([s[-1] for s in contents])
 
 
@@ -27,4 +43,27 @@ print(Y)
 X = np.array(X)
 Y = np.array(Y)
 
-bagging = BaggingClassifier(KNeighborsClassifier(),)
+mIris = KNeighborsClassifier()
+#set with max_sample at .5 nd max feature at .5
+baggingIris = BaggingClassifier(
+                mIris,
+                max_samples=.5,
+                max_features=.5)
+
+mIosphere = KNeighborsClassifier(n_neighbors=3)
+baggingIosphere = BaggingClassifier(
+                mIosphere,
+                max_samples=.5,
+                max_features=2,
+                n_jobs=2,
+                oob_score=True)
+
+#using cross_valication score
+from sklearn.model_selection import cross_val_score
+#bagging the iris dataset
+baggingIrisScore = cross_val_score(baggingIris, Iris_X, Iris_y)
+#print bagging score mean
+print(baggingIrisScore.mean())
+
+
+
